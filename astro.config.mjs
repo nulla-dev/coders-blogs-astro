19269d3f8dde6acc2270daee0808886f5969b4dd
nulla-dev <numjsdev@gmail.com>
2024-12-24 15:08:35 +0530

    base setup - astro-tailwind installed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import expressiveCode from 'astro-expressive-code';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-    integrations: [expressiveCode(), mdx()],
+    site: 'https://numan-dev.web.app/',
+    integrations: [expressiveCode(), mdx(), tailwind()],
 });
