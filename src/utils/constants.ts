b92c53df202c8bcc72ff38c85520126a66941942
nulla-dev <numjsdev@gmail.com>
2024-12-24 15:16:04 +0530

    blog home page

---
+++ src/utils/constants.ts
@@ -0,0 +1,13 @@
+export const site = {
+    title: 'Coders Blogs', // required
+    favicon: '/favicon.svg', // required
+    description: 'Coding Blogs on React, Vue, Angular, Node, Python, Java, C++, C#, Go, Rust, Webpack, Docker, Kubernetes, AWS, GCP, Azure, etc.', // required
+    author: "N.Dev", // required
+    avatar: '/avatar.png', // required
+    motto: 'My Code is more cleaner than my room.',
+    url: 'https://numan-dev.web.app/',
+    recentBlogSize: 25,
+    archivePageSize: 25,
+    postPageSize: 10,
+    feedPageSize: 20,
+}
