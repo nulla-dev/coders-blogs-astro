e41e97417db1a3b857f9a9effb78e1db406b2804
num <mdnmnahmed@gmail.com>
2025-01-04 07:25:56 +0530

    changes

--- src/utils/orderBySticky.ts
+++ src/utils/orderBySticky.ts
@@ -2,11 +2,11 @@ import _ from 'lodash'
 import dayjs from 'dayjs'
 
 export const orderBySticky = (blogs: any) => {
-  let handlePosts = blogs.map((blog: any) => {
+  let modifiedBlogs = blogs.map((blog: any) => {
     blog.sticky = blog.data.sticky ? blog.data.sticky : 0
     blog.dateTimestamp = dayjs(blog.data.date).valueOf()
 
     return blog;
   })
-  return _.orderBy(handlePosts, ['sticky', 'dateTimestamp'], ['desc', 'desc'])
+  return _.orderBy(modifiedBlogs, ['sticky', 'dateTimestamp'], ['desc', 'desc'])
 }
